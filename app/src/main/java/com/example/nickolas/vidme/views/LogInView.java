860035bf0eb5728932cd88c0f00f8174bec7d18d
Nickolas Humeniuk <kolin98111@gmail.com>
2017-09-23 11:44:55 +0300

    Added view for log in page

---
+++ app/src/main/java/com/example/nickolas/vidme/views/LogInView.java
@@ -0,0 +1,11 @@
+package com.example.nickolas.vidme.views;
+
+import com.example.nickolas.vidme.model.entities.User;
+
+
+
+public interface LogInView extends BaseView {
+
+    void createAuth(User user);
+
+}
