75d3fe36b1d3bb60b4ba1533e775d650c9916b26
Nickolas Humeniuk <kolin98111@gmail.com>
2017-09-24 23:28:06 +0300

    Updated api class

--- app/src/main/java/com/example/nickolas/vidme/api/VidmeApi.java
+++ app/src/main/java/com/example/nickolas/vidme/api/VidmeApi.java
@@ -11,10 +11,10 @@ import rx.Observable;
 
 public interface VidmeApi {
     @POST("/auth/create")
-    Call<User> createAuth(@Query("username") String username, @Query("password") String password);
+    Observable<User> createAuth(@Query("username") String username, @Query("password") String password);
 
     @POST("/auth/delete")
-    Call<Boolean> deleteAuth(@Query("AccessToken") String token);
+    Observable<Boolean> deleteAuth(@Query("AccessToken") String token);
 
     @GET("/videos/featured")
     Observable<ResponseBody> featuredVideo(@Query("limit") int limit, @Query("offset") int offset);
@@ -23,5 +23,5 @@ public interface VidmeApi {
     Observable<ResponseBody> newVideo(@Query("limit") int limit, @Query("offset") int offset);
 
     @GET("/videos/feed")
-    Observable<ResponseBody> feedVideo(@Query("limit") int limit, @Query("offset") int offset);
+    Observable<ResponseBody> feedVideo(@Query("limit") int limit, @Query("offset") int offset, @Query("token") String token);
 }
